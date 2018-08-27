076a31b3a4ea811ce1f5675b7870989cc2f496e5
Anthony Mai <maiah2142@gmail.com>
2018-08-27 22:45:02 +1000

    Player movement overhaul

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,31 +1,37 @@
 ﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-	private Rigidbody m_rb;
-	private Transform m_trans;
+	private Rigidbody rb;
+	private Transform trans;
 
 	private const bool DEBUG = true;
 
 	//defines for force
-	private const float pitchForce = 240.0f;
-	private const float rollForce = 240.0f;
-	private const float yawForce = 120.0f;
-	private const float forwardForce = 400.0f;
-	private const float transForce = 200.0f;
+	private const float pitchForce = 6.0f;
+	private const float rollForce = 6.0f;
+	private const float yawForce = 3.0f;
+	private const float forwardForce = 8.0f;
+	private const float backwardForce = 4.0f;
+	private const float transUpForce = 6.0f;
+	private const float transDownForce = 4.0f;
+	private const float transLeftForce = 4.0f;
+	private const float transRightForce = 4.0f;
 
 	private const float pitchClamp = 3.0f;
 	private const float rollClamp = 3.0f;
 	private const float yawClamp = 1.5f;
+
 	//Define
-	private float pitchAxis, rollAxis, yawAxis, surgeAxis, swayAxis, heaveAxis;
+	private float pitchAxis, rollAxis, yawAxis, surgeAxis, swayAxis,
+		heaveAxis;
 
-	private bool keySpaceBrake, keySAS, keyABS;
+	private bool keySAS, keyABS;
 
-	private Vector3 relativeAngularVelocity, relativeTranslationalVelocity;
+	private Vector3 relAngVel, relTranVel;
 
 	private void Start(){
-		m_rb = GetComponent<Rigidbody>();
-		m_trans = GetComponent<Transform>();
+		rb = GetComponent<Rigidbody>();
+		trans = GetComponent<Transform>();
 
 		keySAS = true;
 		keyABS = true;
@@ -42,65 +48,65 @@ public class PlayerMovement : MonoBehaviour {
 		heaveAxis = Input.GetAxisRaw("Heave");
 
 		//grab button input
+		//TODO: Fix this
 		if (Input.GetButton("SAS"))
 			keySAS = !keySAS;
 		if (Input.GetButton("ABS"))
 			keyABS = !keyABS;
-
-		//keySpaceBrake = Input.GetButton("Space Brake");
 	}
 
 	// Update game at fixed intervals
 	private void FixedUpdate(){
-		//update angular vel
-		relativeAngularVelocity = m_trans.InverseTransformDirection(m_rb.angularVelocity);
-		relativeTranslationalVelocity = m_trans.InverseTransformDirection(m_rb.velocity);
+		//update relative rotation and translation velocities
+		relAngVel = trans.InverseTransformDirection(rb.angularVelocity);
+		relTranVel = trans.InverseTransformDirection(rb.velocity);
 
-		ShipRotation(keySAS);
-		ShipThrust(keyABS);
+		ShipRotation();
+		ShipTranslation();
 
 		DisplayDebug();
 	}
 
-	// Rotates ship based on assist mode
-	private void ShipRotation(bool assist){
+	// Rotates ship based on SAS mode
+	private void ShipRotation(){
 		//if SAS mode is on
-		if (assist){
+		if (keySAS){
 			//use SAS "smart" counterthrust
 			/*
-				Vector3 temp = Vector3.left * DeltaForce(pitchForce) * pitchAxis;
-				Vector3.up * DeltaForce(yawForce) * yawAxis);
+				Vector3 temp = Vector3.left * pitchForce
+					* pitchAxis;
+				Vector3.up * yawForce * yawAxis);
 			*/
 			//pitch
 			SAS(pitchAxis,
-				Vector3.right * DeltaForce(CounterForce(0.0f, relativeAngularVelocity.x, pitchForce)),
-				Vector3.left * DeltaForce(SASClamp(pitchClamp, relativeAngularVelocity.x, pitchForce)) * pitchAxis);
+				Vector3.right * CounterForce(0.0f,
+					relAngVel.x, pitchForce),
+				Vector3.left * SASClamp(pitchClamp,
+					relAngVel.x, pitchForce) * pitchAxis);
 			//roll
 			SAS(rollAxis,
-				Vector3.forward * DeltaForce(CounterForce(0.0f, relativeAngularVelocity.z, rollForce)),
-				Vector3.back * DeltaForce(SASClamp(rollClamp, relativeAngularVelocity.z, rollForce)) * rollAxis);
+				Vector3.forward * CounterForce(0.0f, relAngVel.z, rollForce),
+				Vector3.back * SASClamp(rollClamp, relAngVel.z, rollForce) * rollAxis);
 			//yaw
 			SAS(yawAxis,
-				Vector3.up * DeltaForce(CounterForce(0.0f, relativeAngularVelocity.y, yawForce)),
-				Vector3.up * DeltaForce(SASClamp(yawClamp, relativeAngularVelocity.y, yawForce)) * yawAxis);
+				Vector3.up * CounterForce(0.0f, relAngVel.y, yawForce),
+				Vector3.up * SASClamp(yawClamp, relAngVel.y, yawForce) * yawAxis);
 		//if SAS mode is off
 		} else {
 			//no automatic counter thrust
-			m_rb.AddRelativeTorque(Vector3.left * DeltaForce(pitchForce) * pitchAxis);
-			m_rb.AddRelativeTorque(Vector3.back * DeltaForce(rollForce) * rollAxis);
-			m_rb.AddRelativeTorque(Vector3.up * DeltaForce(yawForce) * yawAxis);
+			rb.AddRelativeTorque(Vector3.left * pitchForce * pitchAxis, ForceMode.Force);
+			rb.AddRelativeTorque(Vector3.back * rollForce * rollAxis, ForceMode.Force);
+			rb.AddRelativeTorque(Vector3.up * yawForce * yawAxis, ForceMode.Force);
 		}
 	}
 	// Stability Assist System slowdown
-	private void SAS(float axes, Vector3 retrograde, Vector3 prograde){
+	private void SAS(float axis, Vector3 retrograde, Vector3 prograde){
 		//if no input detected
-		if (axes == 0)
-			//activate retrograde thrusters
-			m_rb.AddRelativeTorque(retrograde);
+		if (axis == 0)
+			rb.AddRelativeTorque(retrograde, ForceMode.Force);
 		//if input detected
 		else
-			//activate prograde thrusters
-			m_rb.AddRelativeTorque(prograde);
+			rb.AddRelativeTorque(prograde, ForceMode.Force);
 	}
 	private float SASClamp(float clamp, float currVel, float force){
 		if (-clamp < currVel && currVel < clamp)
@@ -108,84 +114,104 @@ public class PlayerMovement : MonoBehaviour {
 		return 0.0f;
 	}
 
-	// Translate ship based on assist mode
-	private void ShipThrust(bool assist){
-		//if ABS mode is on
-		if (assist){
-			//use ABS "smart" counterthrust
-			//surge
-			ABS(surgeAxis,
-				Vector3.forward * DeltaForce(CounterForce(0.0f, relativeTranslationalVelocity.z, forwardForce)),
-				Vector3.forward * DeltaForce(forwardForce) * surgeAxis);
-			//sway
-			ABS(swayAxis,
-				Vector3.right * DeltaForce(CounterForce(0.0f, relativeTranslationalVelocity.x, transForce)),
-				Vector3.right * DeltaForce(transForce) * swayAxis);
-			//heave
-			ABS(heaveAxis,
-				Vector3.up * DeltaForce(CounterForce(0.0f, relativeTranslationalVelocity.y, transForce)),
-				Vector3.up * DeltaForce(transForce) * heaveAxis);
+	private void ShipRCS(float axis, Vector3 dirVector, float posForce,
+			float negForce, float relativeDir){
+		//if axis is positive
+		if (axis > 0.0f){
+			rb.AddRelativeTorque(dirVector * posForce * axis,
+				ForceMode.Force);
+		//if axis is negative
+		} else if (axis < 0.0f){
+			rb.AddRelativeForce(dirVector * negForce * axis,
+				ForceMode.Force);
 		}
-		//if ABS mode is off
-		else{
-			//no automatic counter thrust
-			m_rb.AddRelativeForce(Vector3.forward * DeltaForce(forwardForce) * surgeAxis, ForceMode.Force);
-			m_rb.AddRelativeForce(Vector3.right * DeltaForce(transForce) * swayAxis, ForceMode.Force);
-			m_rb.AddRelativeForce(Vector3.up * DeltaForce(transForce) * heaveAxis, ForceMode.Force);
-		}
-	}
-	// Automatic Braking System
-	private void ABS(float axes, Vector3 retrograde, Vector3 prograde){
-		//if no input detected
-		if (axes == 0)
-			//activate retrograde thrusters
-			m_rb.AddRelativeForce(retrograde, ForceMode.Force);
-		//if input detected
-		else
-			//activate prograde thrusters
-			m_rb.AddRelativeForce(prograde, ForceMode.Force);
 	}
 
-	// WIP Space brake system
-	private void SpaceBrake(){
-		m_rb.AddRelativeForce(Vector3.right
-			* DeltaForce(CounterForce(0.0f, relativeTranslationalVelocity.x, transForce)), ForceMode.Force);
-		m_rb.AddRelativeForce(Vector3.up
-			* DeltaForce(CounterForce(0.0f, relativeTranslationalVelocity.y, transForce)), ForceMode.Force);
+	// Translate whole ship with the ShipThrust function
+	private void ShipTranslation(){
+		//forward and back
+		ShipThrust(
+			surgeAxis, //z axis
+			Vector3.forward, //direction vector
+			forwardForce, //positive force
+			backwardForce, //negative force
+			2 //index for z
+		);
+		//right and left
+		ShipThrust(
+			swayAxis, //x axis
+			Vector3.right, //direction vector
+			transRightForce, //positive force
+			transLeftForce, //negative force
+			0 //index for x
+		);
+		//up and down
+		ShipThrust(
+			heaveAxis, //y axis
+			Vector3.up, //direction vector
+			transUpForce, //positive force
+			transDownForce, //negative force
+			1 //index for y
+		);
+	}
+	// Ship thrust for individual axis
+	private void ShipThrust(float axis, Vector3 dirVector, float posForce,
+			float negForce, int relIndex){
+		//if axis is positive
+		if (axis > 0.0f){
+			rb.AddRelativeForce(dirVector * posForce * axis,
+				ForceMode.Force);
+		//if axis is negative
+		} else if (axis < 0.0f){
+			rb.AddRelativeForce(dirVector * negForce * axis,
+				ForceMode.Force);
+		//if no input and ABS system is on
+		} else if (keyABS){
+			//Provide artificial drag to the ship when a thrust 
+			//vector is not used
+			if (relTranVel[relIndex] < posForce / rb.mass 
+					* Time.deltaTime &&
+				relTranVel[relIndex] > negForce / rb.mass 
+					* Time.deltaTime){	
+				Vector3 vel = relTranVel;
+				vel[relIndex] = 0.0f;
+				rb.velocity = transform.TransformDirection(vel);
+			}
+			if (relTranVel[relIndex] > 0){
+				rb.AddRelativeForce(dirVector * negForce * -1,
+					ForceMode.Force);
+			} else if (relTranVel[relIndex] < 0){
+				rb.AddRelativeForce(dirVector * posForce,
+					ForceMode.Force);
+			}
+		}
 	}
 
 	// Helper function for calculating counter force 
 	private float CounterForce(float target, float vel, float force){
 		//if velocity is negative
 		if (vel < target)
-			//return positive force
-			return force;
+			return force; //positive value
 		//if velocity is positve
 		else if (vel > target)
-			//return negative force
-			return -force;
+			return -force; //negative value
 		//if velocity is 0, don't do anything
 		return 0.0f;
 	}
-
-	// Helper function to get force over time
-	private float DeltaForce(float force){
-		return force * Time.deltaTime;
-	}
-
+	
 	// Collection of debug logs
 	private void DisplayDebug(){
-		
-		if (DEBUG) Debug.Log("Speed = " + m_rb.velocity.magnitude + " m/s");
-		if (DEBUG) Debug.Log("ABS = " + keyABS + "\nrelativeTranslationalVelocity = " + relativeTranslationalVelocity);
-		if (DEBUG) Debug.Log("SAS = " + keySAS + "\nrelativeAngularVelocity = " + relativeAngularVelocity);
+		Debug.Log("Speed = " + rb.velocity.magnitude + " m/s");
+		Debug.Log("ABS = " + keyABS);
+		Debug.Log("relTranVel = " + relTranVel);
+		Debug.Log("SAS = " + keySAS);
+		Debug.Log("relAngVel = " + relAngVel);
 
-		float hAngle = Mathf.Atan2(relativeTranslationalVelocity.x, relativeTranslationalVelocity.z) * Mathf.Rad2Deg;
-		float hPy = Mathf.Sqrt(Mathf.Pow(relativeTranslationalVelocity.x, 2) + Mathf.Pow(relativeTranslationalVelocity.z, 2));
-		float vAngle = Mathf.Atan2(relativeTranslationalVelocity.y, hPy) * Mathf.Rad2Deg;
+		float hAngle = Mathf.Atan2(relTranVel.x, relTranVel.z) * Mathf.Rad2Deg;
+		float hPy = Mathf.Sqrt(Mathf.Pow(relTranVel.x, 2) + Mathf.Pow(relTranVel.z, 2));
+		float vAngle = Mathf.Atan2(relTranVel.y, hPy) * Mathf.Rad2Deg;
 
-		if (DEBUG) Debug.Log("hAngle = " + hAngle);
-		if (DEBUG) Debug.Log("vAngle = " + vAngle);
-
+		Debug.Log("hAngle = " + hAngle);
+		Debug.Log("vAngle = " + vAngle);
 	}
 }
---
+++ Assets/Scripts/PlayerTest.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class PlayerTest : MonoBehaviour
+{
+	private Rigidbody rb;
+	private Transform trans;
+
+	private const bool DEBUG = true;
+
+	//defines for force
+	private const float forwardForce = 8.0f;
+	private const float backwardForce = 4.0f;
+
+	private Vector3 relTranVel;
+
+	private float startTime, deltaTime;
+	private bool test;
+
+	private void Start(){
+		startTime = Time.time;
+		rb = GetComponent<Rigidbody>();
+		trans = GetComponent<Transform>();
+		rb.AddRelativeForce(Vector3.forward * 16.0f, ForceMode.Impulse);
+	}
+
+	// Update is called once per frame
+	private void Update(){
+	}
+
+	// Update game at fixed intervals
+	private void FixedUpdate(){
+		//update relative rotation and translation velocities
+		relTranVel = trans.InverseTransformDirection(rb.velocity);
+
+		rb.AddRelativeForce(Vector3.forward * backwardForce * -1, ForceMode.Force);
+
+		DisplayDebug();
+
+		Vector3 orgVel = new Vector3(1.0f, 1.0f, 2.0f);
+		Vector3 testVel, refVel;
+		testVel = Vector3.forward;
+
+
+		if (relTranVel.z < 0 && test == false){
+			test = true;
+			deltaTime = Time.time - startTime;
+		}
+	}
+
+	// Collection of debug logs
+	private void DisplayDebug(){
+		/*
+		Debug.Log("Speed = " + relTranVel);
+		Debug.Log("Time = " + deltaTime);
+		*/
+	}
+}
